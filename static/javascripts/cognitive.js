fd9bda3a930e95c6c8f27ed93370438afd436080
volpino <fox91@anche.no>
2012-11-13 11:26:46 +0100

    almost a complete rewrite, library structure changed

--- static/javascripts/cognitive.js
+++ static/javascripts/cognitive.js
@@ -15,7 +15,7 @@
     else
       return 0;
   }
-  , getAnswers = function (obj) {
+  , extractAnswers = function (obj) {
     var result = {}, tmp, key;
 
     for (key in obj) {
@@ -27,94 +27,92 @@
     return result;
   };
 
-  $.fn.cognitive = function (method) {
-      var methods = {
-        init: function (opt) {
-          this.each(function () {
-            var $element = $(this)
-              , $current
-              , options = $.extend(true, {}, $.fn.cognitive.defaults, opt)
-              , answers = {result: [], score: 0, total: 0, pastTime: 0}
-              , timerId
-              , $timer = $element.find(".cognitive-timer")
-              , nextStep = function ($el) {
-                var next;
-                $el.hide();
-                next = $el.data("next");
-                if (next) {
-                  $current = $(next);
-                  $current.show();
-                  bindStep($current);
-                }
-              }
-              , bindStep = function ($el) {
-                var timeout;
+  var Cognitive = function ($element, opt) {
+    var self = this
+      , defaults = {
+      formSelector: ".cognitive-form",
+      skipSelector: ".cognitive-skip",
+      scoreSelector: ".cognitive-score"
+    };
+    this.$current = null;
+    this.$element = $element;
+    this.options = $.extend(true, {}, defaults, opt);
+    this.answers = {result: [], score: 0, total: 0, pastTime: 0};
+    this.timerId = null;
+    this.$timer = $element.find(".cognitive-timer");
 
-                $el.fadeIn();
+    this.timerId = window.setInterval(function () {
+      self.$timer.text(self.answers.pastTime);
+      self.answers.pastTime += 1;
+    }, 1000);
 
-                $el.find(".cognitive-skip").on("click", function () {
-                  answers.result.push({});
-                  answers.total += 1;
-                  nextStep($el);
-                });
+    this.$element.find(".cognitive-content").children().hide();
+    this.$current = $element.find(this.options.start).eq(0);
+    this.bindStep(this.$current);
 
-                $el.find(".cognitive-form").on("submit", function (e) {
-                  var $this = $(this)
-                    , givenAnswer = $this.serializeObject()
-                    , rightAnswer;
-
-                  e.preventDefault();
-                  e.stopPropagation();
-
-                  answers.result.push(givenAnswer);
-                  rightAnswer = getAnswers($this.data());
-                  if (!$.isEmptyObject(rightAnswer)) {
-                    answers.score += computeScore(givenAnswer, rightAnswer);
-                  }
-                  answers.total += 1;
-                  nextStep($el);
-                  return false;
-                });
-
-                timeout = window.parseFloat($el.data("timeout")) * 1000;
-                if (timeout) {
-                  window.setTimeout(function () {
-                    answers.result.push({});
-                    nextStep($el);
-                  }, timeout);
-                }
-
-                $el.find(".cognitive-score").text(
-                  answers.score + "/" + answers.total + "  (" + answers.pastTime + " sec)"
-                );
-
-                if (!$el.data("next")) {
-                  clearInterval(timerId);
-                }
-              };
-
-            timerId = window.setInterval(function () {
-              $timer.text(answers.pastTime);
-              answers.pastTime += 1;
-            }, 1000);
-            $element.find(".cognitive-content").children().hide();
-            $current = $element.find(options.start).eq(0);
-            bindStep($current);
-          });
-        }
-      };
-
-    if (methods[method])
-      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-    else if (typeof method === 'object' || !method)
-      return methods.init.apply(this, arguments);
-    else
-      $.error('Method ' + method + ' does not exist!');
   };
 
-  $.fn.cognitive.defaults = {
-    formSelector: ".cognitive-form",
-    skipSelector: ".cognitive-skip"
+  Cognitive.prototype.getAnswers = function () {
+    return this.answers;
+  }
+
+  Cognitive.prototype.nextStep = function ($el) {
+    var next;
+    $el.hide();
+    next = $el.data("next");
+    if (next) {
+      this.$current = $(next);
+      this.$current.show();
+      this.bindStep(this.$current);
+    }
+  }
+
+  Cognitive.prototype.bindStep = function ($el) {
+    var timeout
+      , self = this;
+
+    $el.fadeIn();
+
+    $el.find(this.options.skipSelector).on("click", function () {
+      self.answers.result.push({});
+      self.answers.total += 1;
+      self.nextStep($el);
+    });
+
+    $el.find(this.options.formSelector).on("submit", function (e) {
+      var $this = $(this)
+        , givenAnswer = $this.serializeObject()
+        , rightAnswer;
+
+      e.preventDefault();
+      e.stopPropagation();
+
+      self.answers.result.push(givenAnswer);
+      rightAnswer = extractAnswers($this.data());
+      if (!$.isEmptyObject(rightAnswer)) {
+        self.answers.score += computeScore(givenAnswer, rightAnswer);
+      }
+      self.answers.total += 1;
+      self.nextStep($el);
+      return false;
+    });
+
+    timeout = window.parseFloat($el.data("timeout")) * 1000;
+    if (timeout) {
+      window.setTimeout(function () {
+        self.answers.result.push({});
+        self.nextStep($el);
+      }, timeout);
+    }
+
+    $el.find(this.options.scoreSelector).text(
+      self.answers.score + "/" + self.answers.total + "  (" + self.answers.pastTime + " sec)"
+    );
+
+    if (!$el.data("next")) {
+      clearInterval(self.timerId);
+    }
   };
 
+  window.Cognitive = Cognitive;
 }($);
